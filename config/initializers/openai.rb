e7d52e013450a3851dd4b22cbc2372f7085ae6db
mattnice <93342718+mattnice@users.noreply.github.com>
2024-02-11 17:13:59 -0600

    working openai

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,5 +1,5 @@
 OpenAI.configure do |config|
-    config.access_token = "iAVpwvoCv6iNX9ur9Hi8T3BlbkFJ9duwHSn6gcvUNewy3IIt"
+    config.access_token = "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"
     
     # Add any other configurations here
   end
