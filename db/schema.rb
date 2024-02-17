d2dd37b64ff8492929328745df362582e6309314
Peregrin Sadler <peregrinsadler@gmail.com>
2024-02-17 12:04:35 -0600

    Fixed Tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_17_050455) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_17_180203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "personalities", force: :cascade do |t|
-    t.bigint "plant_id"
     t.string "type", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["plant_id"], name: "index_personalities_on_plant_id"
   end
 
   create_table "plants", force: :cascade do |t|
     t.bigint "user_id"
+    t.bigint "personality_id"
     t.string "name", null: false
     t.string "species", null: false
-    t.string "personality", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "wateringTime", null: false
+    t.index ["personality_id"], name: "index_plants_on_personality_id"
     t.index ["user_id"], name: "index_plants_on_user_id"
   end
 
   create_table "prompts", force: :cascade do |t|
     t.bigint "personality_id"
     t.string "text", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["personality_id"], name: "index_prompts_on_personality_id"
   end
 
@@ -52,7 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_17_050455) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "personalities", "plants"
+  add_foreign_key "plants", "personalities"
   add_foreign_key "plants", "users"
   add_foreign_key "prompts", "personalities"
 end
