f5288d52e7981c5667e80f843e57484db4d18548
Peregrin Sadler <peregrinsadler@gmail.com>
2024-02-16 23:26:37 -0600

    Continuing to Debug

--- db/migrate/20240217050442_personality.rb
+++ db/migrate/20240217050442_personality.rb
@@ -1,6 +1,6 @@
 class Personality < ActiveRecord::Migration[7.1]
   def change
-    create_table :personalitys do |t|
+    create_table :personalities do |t|
       t.references  :plant, foreign_key: true
       t.string      :type, null: false
       t.timestamps
