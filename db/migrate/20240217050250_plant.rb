d37380bbf93b494c350829d8779426dec2b06425
Peregrin Sadler <peregrinsadler@gmail.com>
2024-02-17 11:56:55 -0600

    Edited Tables

--- db/migrate/20240217050250_plant.rb
+++ db/migrate/20240217050250_plant.rb
@@ -2,9 +2,10 @@ class Plant < ActiveRecord::Migration[7.1]
   def change
     create_table :plants do |t|
       t.references  :user, foreign_key: true
+      t.references  :personality, foreign_key: true
       t.string      :name, null: false
       t.string      :species, null: false
-      t.string      :personality, null: false
+      t.string      :wateringTime, null: false
     end
   end
 end
--- db/migrate/20240217050442_personality.rb
+++ db/migrate/20240217050442_personality.rb
@@ -1,7 +1,6 @@
 class Personality < ActiveRecord::Migration[7.1]
   def change
     create_table :personalities do |t|
-      t.references  :plant, foreign_key: true
       t.string      :type, null: false
     end
   end
