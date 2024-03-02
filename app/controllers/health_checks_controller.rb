762011ae22e6ba840decccf54782c192640380a4
Peregrin Sadler <peregrinsadler@gmail.com>
2024-03-02 16:40:16 -0600

    Setup a health checks controller and added all the pages

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -1,8 +1,51 @@
 class HealthChecksController < ApplicationController
-    #before_action :set_plant, only: [:show, :edit, :update, :destroy]
+    before_action :set_plant, only: [:show, :edit, :update, :destroy, :index]
     before_action :authenticate_user!
 
     def index
-        @plants = current_user.plants
+        @health_checks = @plant.health_checks
     end
+
+    def show
+    end
+
+    def new
+        @health_check = HealthCheck.new
+    end
+
+    def create
+        @health_check = HealthCheck.new(health_params)
+        @health_check.image.attach(params[:health_check][:image]) if params[:health_check][:image].present?
+        if @health_check.save
+            redirect_to health_check_path
+        else
+            render :new
+        end
+    end
+
+    def update
+        if @health_check.update(health_params)
+            @health_check.image.attach(params[:health_check][:image]) if params[:health_check][:image].present?
+            if @plant.save
+                redirect_to dashboard_path
+            else
+                render :edit
+            end
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @health_check.destroy
+        redirect_to health_check_path
+    end
+
+    def set_plant
+        @plant = Plant.find(params[:plant_id])
+    end
+
+    def health_params
+        params.require(:health_check).permit(:plant_id, :leafColor, :pests, :root, :spots, :other, :image)
+      end
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,5 +3,5 @@ class Plant < ApplicationRecord
     belongs_to  :personality
     has_one_attached :image
     has_one :plant_info
-    has_one :health_check
+    has_many :health_checks
 end
