d4ec883b28f430ed3817d8235eb82a65d075dc7e
Siddharth Maurya <sidmaurya@gmail.com>
2024-03-07 20:23:56 -0600

    Installs sidekiq gem and creates redis image. Establishes Redis connection with the app and uses sidekiq.

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,22 +1,5 @@
 class Reminder < ApplicationRecord
-    # def self.send_reminders
-    #     ReminderMailer.reminder(current_user.email).deliver_now         
-    #     notice: 'Email sent successfully!'
-    # end
-
-    # def self.send_reminders
-    #     puts "send_reminders invoked"
-    #     # sending reminders to active users
-    #     active_users = User.where("last_active_at > ?", 1.hour.ago)
-    
-    #     active_users.each do |user|
-    #       ReminderMailer.reminder(user.email).deliver_now
-    #     end
-    # end
-    
-    # def self.send_reminders
-    #     User.find_each do |user|
-    #         ReminderMailer.reminder(user.email).deliver_now
-    #     end
+    # def send_reminder_email
+    #     ReminderMailer.watering_reminder(user.email, plant).deliver_later
     # end
 end
