7fab97443a4f39ff2e580ff5bef200ab1e3a7656
Peregrin Sadler <peregrinsadler@gmail.com>
2024-03-08 23:02:34 -0600

    Added timezone to user table and registration page. Updated default devise registration controller to also add the first name, last name, and time zone to the user table

--- db/migrate/20240218045508_add_first_and_last_name_to_users.rb
+++ db/migrate/20240218045508_add_first_and_last_name_to_users.rb
@@ -2,5 +2,6 @@ class AddFirstAndLastNameToUsers < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
+    add_column :users, :time_zone, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -108,6 +108,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_02_042705) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "time_zone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
