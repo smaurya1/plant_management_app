aab74017983ead2962d0bd1c26edf57cd6d5ffb8
LEEO0337 <118220073+LEEO0337@users.noreply.github.com>
2024-03-10 16:27:46 -0500

    padding issue for profile icon

--- db/schema.rb
+++ db/schema.rb
@@ -49,8 +49,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_02_042705) do
     t.string "root"
     t.string "spots"
     t.string "other"
-    t.date "currentDay"
-    t.string "doctor"
     t.string "image"
     t.index ["plant_id"], name: "index_health_checks_on_plant_id"
   end
@@ -109,7 +107,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_02_042705) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "time_zone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
